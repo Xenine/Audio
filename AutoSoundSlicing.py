f83f310e02cfcbab3a9fe69fb510dd95efdb0893
Xenine <ivanmyazin99@gmail.com>
2020-07-16 20:28:27 +0300

    Add recursion function to optimize samples time. Add .gitignore file

--- AutoSoundSlicing.py
+++ AutoSoundSlicing.py
@@ -1,35 +1,57 @@
 import scipy.io.wavfile
 
-# This function cannot read wav files with 24-bit data.
-samplerate, data = scipy.io.wavfile.read('raw_45m.wav') 
+enumerator = 0
 
-print(f"number of channels = {data.shape[1]}")
-length = data.shape[0] / samplerate
-print(f"length = {length}s")
-print(f"samplerate = {samplerate}")
-print(f"data.shape = {data.shape}")
+def main():
+	# This function cannot read wav files with 24-bit data.
+	samplerate, data = scipy.io.wavfile.read('raw_21m_ru.wav') # samplerate - how many samples in 1 second, data - the samples themselves
+	number_of_samples = data.shape[0]
+	length = number_of_samples / samplerate
+	silence = 700 # silence wave amplitude
+	min_silence_len = 12500 # 0.3 second
+	
 
-new_data = []
-min_silence_len = 12500 # 0.3 sec
-silence = 700 # Амплитуда волны 
-current = 0
-i = 44100 * 5
-dot = 0
-count = 0
+	print(f"length = {length} seconds")
+	print(f"samplerate = {samplerate}")
+	print(f"data.shape = {number_of_samples}")
 
-while i < data.shape[0] - 1 - min_silence_len:
-	if abs(data[i, 0] < silence):
-		for current in range(0, min_silence_len, 10):
-			if abs(data[i + current + 10, 0]) < silence:
-				pass
+	hypersensitivity(data, min_silence_len, silence, samplerate)
+
+def hypersensitivity(data, min_silence_len, silence, samplerate):
+	global enumerator
+	if min_silence_len <= 8800:
+		enumerator += 1
+		scipy.io.wavfile.write(f"sounds3/{enumerator}_sample.wav", samplerate, data)
+		return
+	start = 0
+	current_point = samplerate * 5
+	
+
+	while current_point < data.shape[0] - samplerate * 5 - 1010:
+		current_silence = 0
+		while True:
+			if abs(data[current_point, 0]) <= silence:
+				current_point += 10
+				current_silence += 10
+				if current_silence == min_silence_len:
+					new_data = data[start:current_point, : ]
+					if new_data.shape[0] >= samplerate * 16:
+						hypersensitivity(new_data, min_silence_len - 250, silence, samplerate)
+					else:
+						enumerator += 1
+						scipy.io.wavfile.write(f"sounds3/{enumerator}_sample.wav", samplerate, new_data)
+					start = current_point
+					current_point += samplerate * 5
+					break
 			else:
-				i += current
 				break
-			if current == min_silence_len - 10:
-				i += current
-				new_data = data[dot:i, :]
-				count += 1
-				scipy.io.wavfile.write(f"sounds2/{count}_45m.wav", samplerate, new_data)
-				dot = i
-				i += 44100 * 5 # 5 sec skip
-	i += 100
+		current_point += 1000
+	new_data = data[start:-1, : ]
+	if new_data.shape[0] >= samplerate * 16:
+		hypersensitivity(new_data, min_silence_len - 250, silence, samplerate)
+	else:
+		enumerator += 1
+		scipy.io.wavfile.write(f"sounds3/{enumerator}_sample.wav", samplerate, new_data)
+
+if __name__ == '__main__':
+	main()
